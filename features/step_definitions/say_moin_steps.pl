0d457634dbdca9d13adfd7dacded2f3d70dcf779
Paul Cochrane <paul@liekut.de>
2014-01-13 21:02:44 +0100

    Regexps should be anchored at the start and the end
    This isn't always necessary but is good practice

--- features/step_definitions/say_moin_steps.pl
+++ features/step_definitions/say_moin_steps.pl
@@ -8,21 +8,21 @@ use Test::BDD::Cucumber::StepFile;
 use Method::Signatures;
 use Capture::Tiny ':all';
 
-Given qr/a MoinSayer object/, func($c) {
+Given qr/^a MoinSayer object$/, func($c) {
     use MoinSayer;
     my $sayer = MoinSayer->new(name => $1);
     ok( $sayer, "Object created" );
     $c->stash->{'scenario'}->{'object'} = $sayer;
 };
 
-When qr/I say "moin" to "(\w+)"/, func($c) {
+When qr/^I say "moin" to "(\w+)"$/, func($c) {
     my $stdout = capture_stdout {
 	$c->stash->{'scenario'}->{'object'}->moin($1);
     };
     $c->stash->{'scenario'}->{'stdout'} = $stdout;
 };
 
-Then qr/I should see the message "moin (\w+)"/, func($c) {
+Then qr/^I should see the message "moin (\w+)"$/, func($c) {
     my $stdout = $c->stash->{'scenario'}->{'stdout'};
     like( $stdout, qr/moin $1/, "Said expected greeting" );
 };
